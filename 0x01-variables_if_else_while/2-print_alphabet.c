428439098428d8106af2e0164f4fd360cee71307
M0HAMEDABDELHAMID <mohamnedabdelhamid0994@gmail.com>
2023-06-17 08:15:40 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * description: print all alphabet letters
+ *
+ * Return: always 0 (succss)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch):
+		ch++:
+	}
+		putchar('\n');
+
+		return (0);
+
